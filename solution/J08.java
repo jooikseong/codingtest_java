e093fd57db71a16f3f8b1109e494ad266abb8243
joo <jooikseong@gmail.com>
2025-07-30 08:57:30 +0900

    test

--- solution/J08.java
+++ solution/J08.java
@@ -2,7 +2,13 @@ import java.util.ArrayDeque;
 
 class J08 {
 
-    private boolean solution(String s) {
+    public static void main(String[] args) {
+        System.out.println(solution3("()()"));
+        System.out.println(solution3("(())()"));
+        System.out.println(solution3("(())())"));
+    }
+
+    private static boolean solution(String s) {
         ArrayDeque<Character> stack = new ArrayDeque<>();
 
         char[] a = s.toCharArray();
@@ -16,6 +22,44 @@ class J08 {
             }
         }
 
+        return stack.isEmpty();
+    }
+
+    private static boolean solution2(String s) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+
+        char[] a = s.toCharArray();
+        for(char c : a) {
+//            System.out.println(stack);
+            if(c == '('){
+                stack.push(c);
+            }
+            else {
+                if(stack.isEmpty() || stack.pop() == ')'){
+                    return false;
+                }
+//                if (stack.isEmpty()) {
+//                    return false;
+//                }
+//                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    private static boolean solution3(String s) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        char[] a = s.toCharArray();
+        for(char c:a) {
+            if(c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
         return stack.isEmpty();
     }
 
