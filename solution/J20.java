4873e536c379baca0683f3f23ff980e44646a10e
jis <jooikseong@gmail.com>
2026-01-05 18:40:23 +0900

    문제 추가

--- solution/J20.java
+++ solution/J20.java
@@ -1,32 +1,33 @@
 import java.util.HashMap;
 
-// 문제 20 완주하지 못한 선수 - 해시
+// 할인 행사 - 해시
 public class J20 {
     /*
     문제 설명
-    수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
+    XYZ 마트는 일정한 금액을 지불하면 10일 동안 회원 자격을 부여합니다. XYZ 마트에서는 회원을 대상으로 매일 한 가지 제품을 할인하는 행사를 합니다. 할인하는 제품은 하루에 하나씩만 구매할 수 있습니다. 알뜰한 정현이는 자신이 원하는 제품과 수량이 할인하는 날짜와 10일 연속으로 일치할 경우에 맞춰서 회원가입을 하려 합니다.
 
-    마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+    예를 들어, 정현이가 원하는 제품이 바나나 3개, 사과 2개, 쌀 2개, 돼지고기 2개, 냄비 1개이며, XYZ 마트에서 14일간 회원을 대상으로 할인하는 제품이 날짜 순서대로 치킨, 사과, 사과, 바나나, 쌀, 사과, 돼지고기, 바나나, 돼지고기, 쌀, 냄비, 바나나, 사과, 바나나인 경우에 대해 알아봅시다. 첫째 날부터 열흘 간에는 냄비가 할인하지 않기 때문에 첫째 날에는 회원가입을 하지 않습니다. 둘째 날부터 열흘 간에는 바나나를 원하는 만큼 할인구매할 수 없기 때문에 둘째 날에도 회원가입을 하지 않습니다. 셋째 날, 넷째 날, 다섯째 날부터 각각 열흘은 원하는 제품과 수량이 일치하기 때문에 셋 중 하루에 회원가입을 하려 합니다.
+
+    정현이가 원하는 제품을 나타내는 문자열 배열 want와 정현이가 원하는 제품의 수량을 나타내는 정수 배열 number, XYZ 마트에서 할인하는 제품을 나타내는 문자열 배열 discount가 주어졌을 때, 회원등록시 정현이가 원하는 제품을 모두 할인 받을 수 있는 회원등록 날짜의 총 일수를 return 하는 solution 함수를 완성하시오. 가능한 날이 없으면 0을 return 합니다.
 
     제한사항
-    마라톤 경기에 참여한 선수의 수는 1명 이상 100,000명 이하입니다.
-    completion의 길이는 participant의 길이보다 1 작습니다.
-    참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다.
-    참가자 중에는 동명이인이 있을 수 있습니다.
+    1 ≤ want의 길이 = number의 길이 ≤ 10
+    1 ≤ number의 원소 ≤ 10
+    number[i]는 want[i]의 수량을 의미하며, number의 원소의 합은 10입니다.
+    10 ≤ discount의 길이 ≤ 100,000
+    want와 discount의 원소들은 알파벳 소문자로 이루어진 문자열입니다.
+    1 ≤ want의 원소의 길이, discount의 원소의 길이 ≤ 12
     입출력 예
-    participant	completion	return
-    ["leo", "kiki", "eden"]	["eden", "kiki"]	"leo"
-    ["marina", "josipa", "nikola", "vinko", "filipa"]	["josipa", "filipa", "marina", "nikola"]	"vinko"
-    ["mislav", "stanko", "mislav", "ana"]	["stanko", "ana", "mislav"]	"mislav"
+    want	number	discount	result
+    ["banana", "apple", "rice", "pork", "pot"]	[3, 2, 2, 2, 1]	["chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"]	3
+    ["apple"]	[10]	["banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana"]	0
     입출력 예 설명
-    예제 #1
-    "leo"는 참여자 명단에는 있지만, 완주자 명단에는 없기 때문에 완주하지 못했습니다.
+    입출력 예 #1
 
-    예제 #2
-    "vinko"는 참여자 명단에는 있지만, 완주자 명단에는 없기 때문에 완주하지 못했습니다.
+    문제 예시와 같습니다.
+    입출력 예 #2
 
-    예제 #3
-    "mislav"는 참여자 명단에는 두 명이 있지만, 완주자 명단에는 한 명밖에 없기 때문에 한명은 완주하지 못했습니다.
+    사과가 할인하는 날이 없으므로 0을 return 합니다.
     * */
 
     public int solution(String[] want, int[] number, String[] discount) {
